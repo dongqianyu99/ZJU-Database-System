061804dd5a5a38b2a36412e8d61f34957844f2b0
dongqianyu99 <dongqianyu99@gmail.com>
2025-05-26 19:51:55 +0800

    IndexMetadata::GetSerializedSize() finished

--- minisql/src/catalog/indexes.cpp
+++ minisql/src/catalog/indexes.cpp
@@ -43,7 +43,14 @@ uint32_t IndexMetadata::SerializeTo(char *buf) const {
  * TODO: Student Implement
  */
 uint32_t IndexMetadata::GetSerializedSize() const {
-  return 0;
+    // Calculate the total size needed for serialization.
+    return sizeof(uint32_t) + // INDEX_METADATA_MAGIC_NUM
+           sizeof(index_id_t) + // index_id_
+           sizeof(uint32_t) + // index_name_
+           index_name_.length() +
+           sizeof(table_id_t) + // table_id_
+           sizeof(uint32_t) + // key count
+           key_map_.size() + sizeof(uint32_t); // key_map_ data
 }
 
 uint32_t IndexMetadata::DeserializeFrom(char *buf, IndexMetadata *&index_meta) {
